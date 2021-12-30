539c9e3d42e8a4021074b1457e0e2ffbcf34c130
shubhangi1208 <ashubhangi1996@gmail.com>
2021-12-30 16:03:23 +0530

    MODIFIED: Timebase Daily Wage Calculated

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,23 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage Builder");
-        int IS_FULL_TIME=1;
-        int EMP_RATE_PER_HOUR=20;
-        int empHours=0;
-        int empWage=0;
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHours = 0;
+        int empWage = 0;
         Random random = new Random();
-        int empCheck= random.nextInt(3);
+        int empCheck = random.nextInt(3);
 
-        if(empCheck== IS_FULL_TIME) {
-            empHours=8;
+        if (empCheck == IS_FULL_TIME) {
+            empHours = 8;
             System.out.println("Employee is present");
-        }
-          else {
-            empHours=0;
+        } else if (empCheck == IS_PART_TIME) {
+            empHours = 4;
+            System.out.println("Part time Employee");
+        } else {
+            empHours = 0;
             System.out.println("Employee is Absent");
-        }
-        empWage=empHours*EMP_RATE_PER_HOUR;
-        System.out.println("The daily wage of an employee is " +empWage);
-    }
 
+        }
+        empWage = empHours * EMP_RATE_PER_HOUR;
+        System.out.println("The daily wage of an employee is " + empWage);
+    }
 }
+
+
