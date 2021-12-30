689625341b9a7e8ec1e3e8feecaf280f781569ce
shubhangi1208 <ashubhangi1996@gmail.com>
2021-12-30 16:19:23 +0530

    MODIFIED: Calculated Daliy wage using SWitch Case

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,31 +1,37 @@
 package com.bridgelabz;
 import java.util.Random;
 public class Main {
-
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage Builder");
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
         int empHours = 0;
         int empWage = 0;
         Random random = new Random();
         int empCheck = random.nextInt(3);
 
-        if (empCheck == IS_FULL_TIME) {
-            empHours = 8;
-            System.out.println("Employee is present");
-        } else if (empCheck == IS_PART_TIME) {
-            empHours = 4;
-            System.out.println("Part time Employee");
-        } else {
-            empHours = 0;
-            System.out.println("Employee is Absent");
-
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHours = 8;
+                System.out.println("Employee Full Time Present");
+                break;
+            case IS_PART_TIME:
+                empHours = 4;
+                System.out.println("Employee Part Time Present");
+                break;
+            default:
+                empHours = 0;
+                System.out.println("Employee is Absent ");
         }
         empWage = empHours * EMP_RATE_PER_HOUR;
-        System.out.println("The daily wage of an employee is " + empWage);
+        System.out.println("The daily wage of employee is:  " + empWage);
     }
 }
+
+
+
+
+
 
 
