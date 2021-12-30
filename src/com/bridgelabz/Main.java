87ed73f290a4faf02df275baca8b50ddff1fae2e
shubhangi1208 <ashubhangi1996@gmail.com>
2021-12-30 16:44:10 +0530

    MODIFIED: Calculated Employee Monthly Wage

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,28 +4,33 @@ public class Main {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NO_OF_WORKING_DAYS=20;
+
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage Builder");
         int empHours = 0;
         int empWage = 0;
+        int totalEmployeeWage=0;
+        int totalWorkingDays=0;
+        int totalEmphours=0;
         Random random = new Random();
         int empCheck = random.nextInt(3);
 
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHours = 8;
-                System.out.println("Employee Full Time Present");
-                break;
-            case IS_PART_TIME:
-                empHours = 4;
-                System.out.println("Employee Part Time Present");
-                break;
-            default:
-                empHours = 0;
-                System.out.println("Employee is Absent ");
+        for(int i=1;i<=NO_OF_WORKING_DAYS;i++) {
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHours = 4;
+                    break;
+                default:
+                    empHours = 0;
+            }
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            totalEmployeeWage=empWage+totalEmployeeWage;
         }
-        empWage = empHours * EMP_RATE_PER_HOUR;
-        System.out.println("The daily wage of employee is:  " + empWage);
+        System.out.println("Monthly wage of employee is:  " +totalEmployeeWage);
     }
 }
 
