4765cb369a76bfe6434c246cb391f16859bf2d0b
shubhangi1208 <ashubhangi1996@gmail.com>
2021-12-30 17:02:49 +0530

    MODIFIED: 100 Hours and 20 Days Condition applied

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NO_OF_WORKING_DAYS=20;
+    public static final int TOTAL_WORKING_HOURS=100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage Builder");
@@ -13,10 +14,12 @@ public class Main {
         int totalEmployeeWage=0;
         int totalWorkingDays=0;
         int totalEmphours=0;
-        Random random = new Random();
-        int empCheck = random.nextInt(3);
 
-        for(int i=1;i<=NO_OF_WORKING_DAYS;i++) {
+        while (totalEmphours<=TOTAL_WORKING_HOURS && totalWorkingDays< NO_OF_WORKING_DAYS)
+        {
+            totalWorkingDays++;
+            Random random = new Random();
+            int empCheck = random.nextInt(3);
             switch (empCheck) {
                 case IS_FULL_TIME:
                     empHours = 8;
@@ -29,8 +32,9 @@ public class Main {
             }
             empWage = empHours * EMP_RATE_PER_HOUR;
             totalEmployeeWage=empWage+totalEmployeeWage;
+            totalEmphours+=empHours;
         }
-        System.out.println("Monthly wage of employee is:  " +totalEmployeeWage);
+        System.out.println("The total wage of Employee for 20 days or 100 hours is:  " +totalEmployeeWage);
     }
 }
 
