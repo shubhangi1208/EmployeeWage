e571528dc309f87a07d9a29f6fc2f9a525423122
shubhangi1208 <96242483+shubhangi1208@users.noreply.github.com>
2022-01-09 20:39:15 +0530

    ADDED: Employee Wage Program using OOPS Concept

---
+++ employeewagebuilder.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+public class employeewagebuilder {
+	
+public static final	int IS_FULL_TIME=1;
+public static final int IS_PART_TIME=2;
+public static final int EMP_RATE_PER_HOUR=20;
+public static final int NO_OF_WORKING_DAYS=20;
+public static final int TOTAL_WORKING_HOURS=100;
+int empHours=0;
+int empWage=0;
+int totalEmployeeWage=0;
+int totalWorkingDays=0;
+int totalEmphours=0;
+  private void empWages()
+  {
+	System.out.println("Welcome to employee wage Builder");
+  
+  
+  while (totalEmphours<=TOTAL_WORKING_HOURS && totalWorkingDays< NO_OF_WORKING_DAYS)
+  {
+	  totalWorkingDays++;
+	  Random random = new Random();
+	  int empCheck= random.nextInt(3);
+	  switch (empCheck)
+	  {
+	  case IS_FULL_TIME:
+		  empHours=8;
+		  break;
+	  case IS_PART_TIME:
+		  empHours=4;
+		  break;
+		  default:
+		empHours=0;  
+	  }
+	  empWage=empHours*EMP_RATE_PER_HOUR;
+	  totalEmployeeWage+=empWage;
+      totalEmphours+=empHours;
+  }
+ 
+  System.out.println("The total wage of Employee for 20 days or 100 hours is:  " +totalEmployeeWage);
+  }
+  public static void main(String[] args) {
+	  employeewagebuilder emp=new employeewagebuilder();
+	  emp.empWages();
+	}	
+
+}
