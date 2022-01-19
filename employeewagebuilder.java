52c3d10ca06d4e3fa3f1be9d35aa9475628172f9
shubhangi1208 <96242483+shubhangi1208@users.noreply.github.com>
2022-01-19 12:04:07 +0530

    ADDED: Employee Wage for Multiple Companies

--- employeewagebuilder.java
+++ employeewagebuilder.java
@@ -1,47 +1,48 @@
+package com.bridgelabz;
 import java.util.Random;
 public class employeewagebuilder {
-	
-public static final	int IS_FULL_TIME=1;
-public static final int IS_PART_TIME=2;
-public static final int EMP_RATE_PER_HOUR=20;
-public static final int NO_OF_WORKING_DAYS=20;
-public static final int TOTAL_WORKING_HOURS=100;
-int empHours=0;
-int empWage=0;
-int totalEmployeeWage=0;
-int totalWorkingDays=0;
-int totalEmphours=0;
-  private void empWages()
-  {
-	System.out.println("Welcome to employee wage Builder");
-  
-  
-  while (totalEmphours<=TOTAL_WORKING_HOURS && totalWorkingDays< NO_OF_WORKING_DAYS)
-  {
-	  totalWorkingDays++;
-	  Random random = new Random();
-	  int empCheck= random.nextInt(3);
-	  switch (empCheck)
-	  {
-	  case IS_FULL_TIME:
-		  empHours=8;
-		  break;
-	  case IS_PART_TIME:
-		  empHours=4;
-		  break;
-		  default:
-		empHours=0;  
-	  }
-	  empWage=empHours*EMP_RATE_PER_HOUR;
-	  totalEmployeeWage+=empWage;
-      totalEmphours+=empHours;
-  }
- 
-  System.out.println("The total wage of Employee for 20 days or 100 hours is:  " +totalEmployeeWage);
-  }
-  public static void main(String[] args) {
-	  employeewagebuilder emp=new employeewagebuilder();
-	  emp.empWages();
-	}	
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
 
+
+    public static void empWage(String company, int wagePerHr,int numWorkingDays,int maxHrPerMonth) {
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+        while( totalEmpHrs <= maxHrPerMonth && totalWorkingDays < numWorkingDays) {
+            totalWorkingDays++;
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+
+            switch ((int) empCheck) {
+                case IS_PART_TIME:
+                    System.out.println("Employee is Present as PART TIME");
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Present as FULL TIME");
+                    empHrs = 8;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    empHrs = 0;
+                    break;
+            }
+            totalEmpHrs += empHrs;
+            System.out.println("Day No :"+ totalWorkingDays +" Emp Hrs: "+ empHrs);
+        }
+        int totalEmpWage = totalEmpHrs * wagePerHr;
+        System.out.println("Total Employee Wage for Company : "+ company +" is : "+totalEmpWage);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wages Program.");
+        empWage("Atos Syntel",30,20,100);
+       empWage("TCS",40,20,100);
+    }
 }
+
+
+
+
+
+
+
